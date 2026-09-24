bc132faa3feee627d3e7e7ff8c37b45135eaa54f
T0mpans <19tomek11@gmail.com>
2026-09-24 10:37:10 +0200

    widok + pobranie id

--- app/src/main/java/com/example/retrofit_aplikacja/MainActivity.java
+++ app/src/main/java/com/example/retrofit_aplikacja/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.retrofit_aplikacja;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button buttonNastepne;
+    RadioButton radioButtonA, radioButtonB, radioButtonC;
+    RadioGroup radioGroupPytania;
+    TextView textViewTresc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        buttonNastepne = findViewById(R.id.button);
+        radioButtonA = findViewById(R.id.radioButton);
+        radioButtonB = findViewById(R.id.radioButton2);
+        radioButtonC = findViewById(R.id.radioButton3);
+        textViewTresc = findViewById(R.id.textViewPytanie);
+        radioGroupPytania = findViewById(R.id.radioGroupPytania);
     }
 }
